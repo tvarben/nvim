4e390775de734470feeca4e9f4fafb3e44b52f60
tvarben@tvarben-HP-EliteBook-850-G6 <tvaarben@gmail.com>
2025-10-22 19:10:45 +0200

    updated some stuff

--- lua/config/clipboard.lua
+++ lua/config/clipboard.lua
@@ -1,18 +1,3 @@
--- vim.opt.clipboard = "unnamedplus"
--- vim.g.clipboard = {
---   name = "win32yank",
---   copy = {
---     ["+"] = "win32yank.exe -i --crlf",
---     ["*"] = "win32yank.ext -i --crlf",
---
---   },
---   paste = {
---     ["+"] = "win32yank.exe -o --lf",
---     ["*"] = "win32yank.exe -o --lf",
---   },
---   cache_enabled = 0,
--- }
-
 vim.opt.clipboard = "unnamedplus"
 vim.g.clipboard = {
   name = "xclip",
@@ -27,18 +12,3 @@ vim.g.clipboard = {
   },
   cache_enabled = 0,
 }
-
--- vim.opt.clipboard = "wl-copy"
--- vim.g.clipboard = {
---   name = "wl-copy",
---   copy = {
---     ["+"] = "wl-copy -n",
---     ["*"] = "wl-copy -n",
---
---   },
---   paste = {
---     ["+"] = "wl-paste -n",
---     ["*"] = "wl-paste -n" ,
---   },
---   cache_enabled = 0,
--- }
--- lua/plugins/lspzero.lua
+++ lua/plugins/lspzero.lua
@@ -73,8 +73,8 @@ return {
       vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
       vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
       vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-      -- vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-      vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+      -- vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', optse)
+      vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts) 
       vim.keymap.set('n', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
         opts)
       vim.keymap.set('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
